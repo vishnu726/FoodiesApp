f4c450e3fe7fc649dbe3a8911d7006b8dfca4f4b
jonnyb <jon.burgoyne1@gmail.com>
2018-08-22 16:26:48 -0600

    Initial Commit

---
+++ GoodEatins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodEatins
+//
+//  Created by jonnyb on 8/22/18.
+//  Copyright © 2018 jonnyb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
